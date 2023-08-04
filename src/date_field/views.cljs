6aaf3cd00dab55ed2da21e14f24ce7f0aae3db58
abhinav omprakash <abhinav@abhinavs-MacBook-Air.local>
2023-08-04 21:57:39 +0530

    add submit button

--- src/date_field/views.cljs
+++ src/date_field/views.cljs
@@ -1,8 +1,16 @@
 (ns date-field.views)
 
+
+(defn submit-button
+  []
+  [:button {:class ["button" "my-3"  "is-dark"]}
+   "submit"])
+
+
 (defn main-panel
   []
   [:div {:class ["container" "my-6"]}
    [:input {:class ["input"]
             :type "text"
-            :placeholder "DD-MM-YYYY"}]])
+            :placeholder "DD-MM-YYYY"}]
+   [submit-button]])
