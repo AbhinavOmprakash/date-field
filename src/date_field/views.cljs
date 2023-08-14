3d48a9fab71c6e277f6b65c3441389b022a0d57c
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2023-08-14 22:06:40 +0530

    Move all state reads to inidividual components

--- src/date_field/views.cljs
+++ src/date_field/views.cljs
@@ -6,79 +6,29 @@
 (defonce subscriptions (atom {}))
 
 
+(def panel-state
+  (atom {:date-field ""
+         :date-range {:start ""
+                      :end ""}
+         :errors  {:date-field false
+                   :date-range {:start false
+                                :end false}}}))
+
+
 (defn subscribe
   [key_ ks]
-  (let [atm (r/atom nil)]
+  (let [f #(get-in % ks)
+        atm (r/atom (f @panel-state))] ; initialize atom with state from panel-state
+
     ;; check if a subscription is already registered because react rerenders componnents, and you don't want to register 
     ;; a new subscription every time
     (if (contains? @subscriptions key_)
       (get-in @subscriptions [key_ :state])
-      (do (swap! subscriptions assoc key_ {:f #(get-in % ks)
-                                           :state atm})
-          atm))))
+      (do  (swap! subscriptions assoc key_ {:f #(get-in % ks)
+                                            :state atm})
+           atm))))
 
-
-(defn submit-button
-  [{:keys [enabled? on-click]
-    :or {enabled? true
-         on-click identity}}]
-  (prn "submit-button")
-  [:button {:class ["button" "my-3"  "is-dark"]
-            :disabled (not enabled?)
-            :on-click on-click}
-   "submit"])
-
-
-(defn valid-date-format?
-  [date]
-  (or (empty? date)
-      ;; FYI this is a bad way to validate dates.
-      ;; you should rely on your underlying date-time library
-      ;; for this validation but for the blogpost this is
-      ;; good enough
-      (boolean (re-matches #"[\d]{2}\-[\d]{2}\-[\d]{4}" date))))
-
-
-(defn date-field
-  [{:keys [on-error on-error-resolved on-input value]
-    :or {on-error identity
-         on-error-resolved identity
-         on-input identity}}]
-  (fn [{:keys [value]}]
-    (prn "date field" value)
-    (let [valid-input? (valid-date-format? value)]
-      (if valid-input? (on-error-resolved) (on-error))
-      [:div [:input {:class ["input" (if valid-input? nil "is-danger")]
-                     :type "text"
-                     :value value
-                     :placeholder "DD-MM-YYYY"
-                     :on-input (fn [x] (on-input (-> x .-target .-value)))}]
-       (when (not valid-input?)
-         [:p {:class "help is-danger"} "incorrect input date format, use DD-MM-YYYY"])])))
-
-
-(defn date-range
-  [{:keys [start
-           end
-           on-input
-           on-error
-           on-error-resolved]}]
-  (prn "date range" start end)
-  [:div {:class ["my-6" "is-flex is-align-items-center "]}
-   [:p {:class "pr-3"} "from"]
-   [:span {:class "pr-3"}
-    [date-field {:value start
-                 :on-input (fn [x] (on-input #(assoc % :start x)))
-                 :on-error (fn [] (on-error #(assoc % :start true)))
-                 :on-error-resolved (fn [] (on-error #(assoc % :start false)))}]]
-   [:p {:class "pr-3"} "to"]
-   [:span {:class "pr-3"}
-    [date-field {:value end
-                 :on-input (fn [x] (on-input #(assoc % :end x)))
-                 :on-error (fn [] (on-error #(assoc % :end true)))
-                 :on-error-resolved (fn [] (on-error #(assoc % :end false)))}]]])
-
-
+;; not important
 (defn contains-errors?
   [m]
   (let [child-nodes (fn this
@@ -98,13 +48,67 @@
     (not (every? false? (child-nodes m)))))
 
 
-(def panel-state
-  (atom {:date-field ""
-         :date-range {:start ""
-                      :end ""}
-         :errors  {:date-field false
-                   :date-range {:start false
-                                :end false}}}))
+(defn submit-button
+  [{:keys [id subscription-path enabled? on-click]
+    :or {enabled? true
+         on-click identity}}]
+  (let [errors @(subscribe id subscription-path)          ;; new
+        enabled? (not (contains-errors? errors))]         ;; new
+    [:button {:class ["button" "my-3"  "is-dark"]
+              :disabled (not enabled?)
+              :on-click on-click}
+     "submit"]))
+
+
+(defn valid-date-format?
+  [date]
+  (or (empty? date)
+      ;; FYI this is a bad way to validate dates.
+      ;; you should rely on your underlying date-time library
+      ;; for this validation but for the blogpost this is
+      ;; good enough
+      (boolean (re-matches #"[\d]{2}\-[\d]{2}\-[\d]{4}" date))))
+
+
+(defn date-field
+  [{:keys [id subscription-path on-error on-error-resolved on-input]
+    :or {on-error identity
+         on-error-resolved identity
+         on-input identity}}]
+  (fn []
+    (let [value @(subscribe id subscription-path)                                            ;; new
+          valid-input? (valid-date-format? value)]
+      (if valid-input? (on-error-resolved) (on-error))
+      [:div [:input {:class ["input" (if valid-input? nil "is-danger")]
+                     :type "text"
+                     :value value
+                     :placeholder "DD-MM-YYYY"
+                     :on-input (fn [x] (on-input (-> x .-target .-value)))}]
+       (when (not valid-input?)
+         [:p {:class "help is-danger"} "incorrect input date format, use DD-MM-YYYY"])])))
+
+
+(defn date-range
+  [{:keys [on-input
+           on-error
+           id
+           subscription-path
+           on-error-resolved]}]
+  [:div {:class ["my-6" "is-flex is-align-items-center "]}
+   [:p {:class "pr-3"} "from"]
+   [:span {:class "pr-3"}
+    [date-field {:id (keyword (str (name id) "-" "start"))                                ;; new
+                 :subscription-path (conj subscription-path :start)                       ;; new
+                 :on-input (fn [x] (on-input #(assoc % :start x)))
+                 :on-error (fn [] (on-error #(assoc % :start true)))
+                 :on-error-resolved (fn [] (on-error-resolved #(assoc % :start false)))}]]
+   [:p {:class "pr-3"} "to"]
+   [:span {:class "pr-3"}
+    [date-field {:id (keyword (str (name id) "-" "end"))                                  ;; new
+                 :subscription-path (conj subscription-path :end)                         ;; new
+                 :on-input (fn [x] (on-input #(assoc % :end x)))
+                 :on-error (fn [] (on-error #(assoc % :end true)))
+                 :on-error-resolved (fn [] (on-error-resolved #(assoc % :end false)))}]]])
 
 
 (defn watch-and-call-subscriptions
@@ -119,21 +123,23 @@
 (add-watch panel-state :watch-global-state watch-and-call-subscriptions)
 
 
+;; now the main-panel doesn't reference any atom!
 (defn main-panel
   []
   (fn []
     [:div {:class ["container" "my-6"]}
      [date-field {:on-error (fn [] (swap! panel-state assoc-in [:errors :date-field] true))
                   :on-error-resolved (fn [] (swap! panel-state assoc-in [:errors :date-field] false))
-                  :value @(subscribe :date-field [:date-field])                                            ; atom 1
+                  :id :date-field
+                  :subscription-path [:date-field]
                   :on-input (fn [x] (swap! panel-state assoc :date-field x))}]
-     [date-range {:start @(subscribe :date-range-start [:date-range :start])                              ; atom 2
-                  :end   @(subscribe :date-range-start [:date-range :end])                                ; atom 3
+     [date-range {:id :date-range
+                  :subscription-path [:date-range]
                   :on-input (fn [f] (swap! panel-state update :date-range f))
                   :on-error (fn [f] (swap! panel-state update-in [:errors :date-range] f))
                   :on-error-resolved (fn [f] (swap! panel-state update-in [:errors :date-range] f))}]
-     [submit-button {:enabled? (not (contains-errors? @(subscribe :errors [:errors])))                     ; atom 4
+     [submit-button {:id :submit-button
+                     :subscription-path [:errors]
                      :on-click #(reset! panel-state {:date-field ""
                                                      :date-range {:start ""
                                                                   :end ""}})}]]))
-
