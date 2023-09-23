c222dc33d0cc02c8ef0c01a88343d61b0ae8248c
AbhinavOmprakash <abhinav.omprakash10@gmail.com>
2023-09-23 14:35:46 +0530

    date field validation works

--- src/Main.elm
+++ src/Main.elm
@@ -1,25 +1,57 @@
 module Main exposing (..)
+import Debug exposing (log) 
+import Regex
 import Browser
-import Html exposing (Html, button, div, text, input)
+import Html exposing (Html, button, div, text, input, p)
 import Html.Events exposing (onClick, onInput)
 import Html.Attributes exposing (value, placeholder, class)
 
 
 type alias Model =
-  { text : String }
+  { date : String
+  , dateValidtion: DateStatus}
 
-type Msg = Clicked String
+type DateStatus 
+  = EmptyDate
+  | ValidDate 
+  | InvalidDateFormat
 
+type Msg 
+  = UpdateDate String
+
+isValidDateFormat: String -> Bool
+isValidDateFormat s =
+  Regex.contains (Maybe.withDefault Regex.never <| Regex.fromString "[\\d]{4}\\-[\\d]{2}\\-[\\d]{2}" ) s
+
+
+validateDate: Model -> Model
+validateDate model =
+  let 
+      dateStatus =
+        if model.date == "" then 
+          EmptyDate
+        else if not (isValidDateFormat model.date) then
+          InvalidDateFormat
+        else 
+          ValidDate
+  in 
+    {model | dateValidtion = dateStatus}
+
+update : Msg -> Model -> Model 
 update msg model =
   case msg of
-    Clicked newText -> 
-      { model | text = newText}
+    UpdateDate newDate -> 
+      validateDate { model | date = newDate }
 
+dateField: Model -> Html Msg
 dateField model=
-  input [placeholder "YYYY-MM-DD"
-         , class "input"
-         , onInput Clicked
-         , value model.text] []
+  div [] 
+      [input [placeholder "YYYY-MM-DD"
+             , class "input"
+             , onInput UpdateDate
+             , value model.date] [],
+       p [][ if model.dateValidtion == InvalidDateFormat then text "InvalidDateFormat, use YYYY-MM-DD" 
+             else text ""]]
 
 submitButton =
   button [class "button is-dark"][text "submit"]
@@ -30,4 +62,4 @@ view model =
 
 
 main =
-  Browser.sandbox {init = {text = ""}, update = update, view = view}
+  Browser.sandbox {init = { date = "", dateValidtion = EmptyDate}, update = update, view = view}
